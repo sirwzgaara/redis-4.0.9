b6b4d1e227ca700a5dba2b15b614041efb7a5cb9
e450c <1819644289@qq.com>
2018-07-06 09:26:41 +0800

    adjust format

--- src/adlist.c
+++ src/adlist.c
@@ -44,11 +44,13 @@ list *listCreate(void)
 
     if ((list = zmalloc(sizeof(*list))) == NULL)
         return NULL;
-    list->head = list->tail = NULL;
-    list->len = 0;
-    list->dup = NULL;
-    list->free = NULL;
+	
+    list->head 	= list->tail = NULL;
+    list->len  	= 0;
+    list->dup   = NULL;
+    list->free  = NULL;
     list->match = NULL;
+	
     return list;
 }
 
@@ -60,12 +62,15 @@ void listEmpty(list *list)
 
     current = list->head;
     len = list->len;
-    while(len--) {
+    while(len--) 
+	{
         next = current->next;
-        if (list->free) list->free(current->value);
+        if (list->free) 
+			list->free(current->value);
         zfree(current);
         current = next;
     }
+	
     list->head = list->tail = NULL;
     list->len = 0;
 }
@@ -91,11 +96,15 @@ list *listAddNodeHead(list *list, void *value)
 
     if ((node = zmalloc(sizeof(*node))) == NULL)
         return NULL;
+	
     node->value = value;
-    if (list->len == 0) {
+    if (list->len == 0) 
+	{
         list->head = list->tail = node;
         node->prev = node->next = NULL;
-    } else {
+    } 
+	else 
+	{
         node->prev = NULL;
         node->next = list->head;
         list->head->prev = node;
@@ -117,11 +126,15 @@ list *listAddNodeTail(list *list, void *value)
 
     if ((node = zmalloc(sizeof(*node))) == NULL)
         return NULL;
+	
     node->value = value;
-    if (list->len == 0) {
+    if (list->len == 0) 
+	{
         list->head = list->tail = node;
         node->prev = node->next = NULL;
-    } else {
+    } 
+	else 
+	{
         node->prev = list->tail;
         node->next = NULL;
         list->tail->next = node;
@@ -131,7 +144,8 @@ list *listAddNodeTail(list *list, void *value)
     return list;
 }
 
-list *listInsertNode(list *list, listNode *old_node, void *value, int after) {
+list *listInsertNode(list *list, listNode *old_node, void *value, int after) 
+{
     listNode *node;
 
     if ((node = zmalloc(sizeof(*node))) == NULL)
@@ -192,7 +206,8 @@ listIter *listGetIterator(list *list, int direction)
 {
     listIter *iter;
 
-    if ((iter = zmalloc(sizeof(*iter))) == NULL) return NULL;
+    if ((iter = zmalloc(sizeof(*iter))) == NULL) 
+		eturn NULL;
     if (direction == AL_START_HEAD)
         iter->next = list->head;
     else
@@ -235,7 +250,8 @@ listNode *listNext(listIter *iter)
 {
     listNode *current = iter->next;
 
-    if (current != NULL) {
+    if (current != NULL) 
+	{
         if (iter->direction == AL_START_HEAD)
             iter->next = current->next;
         else
@@ -260,26 +276,34 @@ list *listDup(list *orig)
 
     if ((copy = listCreate()) == NULL)
         return NULL;
-    copy->dup = orig->dup;
-    copy->free = orig->free;
+    copy->dup   = orig->dup;
+    copy->free  = orig->free;
     copy->match = orig->match;
     listRewind(orig, &iter);
-    while((node = listNext(&iter)) != NULL) {
+	
+    while((node = listNext(&iter)) != NULL) 
+	{
         void *value;
 
-        if (copy->dup) {
+        if (copy->dup) 
+		{
             value = copy->dup(node->value);
-            if (value == NULL) {
+            if (value == NULL) 
+			{
                 listRelease(copy);
                 return NULL;
             }
-        } else
+        } 
+		else
             value = node->value;
-        if (listAddNodeTail(copy, value) == NULL) {
+		
+        if (listAddNodeTail(copy, value) == NULL) 
+		{
             listRelease(copy);
             return NULL;
         }
     }
+	
     return copy;
 }
 
